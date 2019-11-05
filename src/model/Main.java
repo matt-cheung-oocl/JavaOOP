54d0a974797e6ccd4fdefcbb3355fcfbe5d90e42
matt-cheung-oocl <57204231+matt-cheung-oocl@users.noreply.github.com>
2019-11-05 12:57:15 +0800

    Add files via upload

---
+++ src/model/Main.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Main {
+
+	public static void main(String [] args){
+		Student student1 = new Student("matt", 18, "male", 1234);
+		System.out.println(student1.toString());
+	}
+}
